47cc3ca9f3b57e6414b4abfabb2f99878a245920
mladsijkapitan <mladsijkapitan@gmail.com>
2026-04-19 14:09:21 +0300

    change drop cooldown to 4 hours

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN: str = os.environ["BOT_TOKEN"]
 DATABASE_URL: str = os.environ["DATABASE_URL"]
 ADMIN_IDS: list[int] = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",") if x]
 
-DROP_COOLDOWN_HOURS: int = 8
+DROP_COOLDOWN_HOURS: int = 4
 DROP_MAX_PER_DAY: int = 3
 TRADE_EXPIRE_HOURS: int = 24
 TRADE_MAX_ACTIVE: int = 5
